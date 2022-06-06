5c4df430dbf896dcfdb7b6a873f190c3d0e142e0
Lucy Linder <lucy.derlin@gmail.com>
2022-06-06 07:53:23 +0200

    first plot

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import * as dfd from "danfojs";
+import Plot from 'react-plotly.js';
+
+const books = new dfd.DataFrame(require('./data/all.json'));
+
+
+const byday1 = books.groupby(['date']).agg({'minutes': 'sum'})
+
+function f(x){
+  return new dfd.DataFrame({
+    minutes_sum: [x['minutes'].sum()],
+    tasks: [x['task'].unique().values.join(", ")],
+  });
+}
+
+let byday = books.groupby(['date'])
+byday = byday.apply(x => f(x))
+byday.print()
+// assuming that sourceDf has columns ['key', 'col1', 'col2']
+/*const groupedDf = books.groupBy('date');
+
+const __groups__ = require('../node_modules/dataframe-js/lib/symbol').__groups__;
+const complexAggregateDf = new DataFrame(Object.values(groupedDf[__groups__]).map(({groupKey, group}) => ({
+  ...groupKey,
+  'minutes_sum': group.stat.sum('minutes'),
+  'tasks': group.tasks,
+}), [...groupedDf.on, 'minutes_sum', 'tasks']));*/
+
+//books.sql.register('books')
+//DataFrame.sql.registerTable(books, 'books')
+// Request on Table
+//const byday1 = DataFrame.sql.request('SELECT date FROM books GROUP BY date')
+//console.log('xx', byday)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Plot
+        data={[
+          {
+            x: byday['date_Group'].values,
+            y: byday['minutes_sum'].values,
+            type: 'line',
+            mode: 'lines',
+            text: byday['tasks'].values,
+          }
+        ]}
+        style={{width: '100%', height: '100%'}}
+        layout={{title: 'A Fancy Plot'}}
+        useResizeHandler={true}
+      />
   );
 }
 
