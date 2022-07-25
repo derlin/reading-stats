7de2b59a5b05ab730f0755dfd92afac930a016d0
Lucy Linder <lucy.derlin@gmail.com>
2022-07-25 16:20:04 +0200

    fix: use svg icon for scroll to top button

--- src/components/ScrollToTopButton.js
+++ src/components/ScrollToTopButton.js
@@ -8,16 +8,13 @@ const Button = styled.div`
   transform: translateX(-50%);
   bottom: 1em;
   /* make it round and small */
+  border-radius: 50%;
   width: 40px;
   height: 40px;
-  text-align: center;
-  font-size: 22px;
-  padding: 5px;
-  /* colors */
-  border: 1px solid white;
-  border-radius: 50%;
-  background-color: #673ab7;
-  color: white;
+  /* background arrow */
+  background-color: white;
+  background-image: url('data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjRweCIgaGVpZ2h0PSIyNHB4IiB2aWV3Qm94PSIwIDAgMjQgMjQiIGZpbGw9Im5vbmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CjxwYXRoIGQ9Ik0xNS43NSAxMkwxMiA4LjI1TTEyIDguMjVMOC4yNSAxMk0xMiA4LjI1VjE1Ljc1TTIxLjc1IDEyQzIxLjc1IDE3LjM4NDggMTcuMzg0OCAyMS43NSAxMiAyMS43NUM2LjYxNTIyIDIxLjc1IDIuMjUgMTcuMzg0OCAyLjI1IDEyQzIuMjUgNi42MTUyMiA2LjYxNTIyIDIuMjUgMTIgMi4yNUMxNy4zODQ4IDIuMjUgMjEuNzUgNi42MTUyMiAyMS43NSAxMloiIHN0cm9rZT0iIzY3M0FCNyIgc3Ryb2tlLXdpZHRoPSIxLjYiIHN0cm9rZS1saW5lY2FwPSJyb3VuZCIgc3Ryb2tlLWxpbmVqb2luPSJyb3VuZCIvPgo8L3N2Zz4K');
+  background-size: contain;
   /* other */
   cursor: pointer;
   z-index: 1;
@@ -48,11 +45,7 @@ const ScrollToTopButton = () => {
 
   window.addEventListener('scroll', toggleVisible);
 
-  return (
-    <Button style={{ display: visible ? 'inline' : 'none' }}>
-      <span onClick={scrollToTop}>⬆</span>
-    </Button>
-  );
+  return <Button style={{ display: visible ? 'inline' : 'none' }} onClick={scrollToTop}></Button>;
 };
 
 export default ScrollToTopButton;
